1ed0326296eb857d4d35ad68bc8ef6df383dd615
Arshad-97 <agmarshad97@gmail.com>
2020-10-19 18:22:04 +0530

    Step 2: PHP  Emped with html

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php 
-    echo "hello world";
+<html>
+
+<head>
+    <title>
+        My first php page
+    </title>
+</head>
+
+<body>
+    This is html body 
+
+    <body>
+
+        <?php
+echo "This is PHP body"
 ?>
+    </body>
+
+</body>
+
+</html>
